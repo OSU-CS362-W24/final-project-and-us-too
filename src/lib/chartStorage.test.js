349f72de1880031d7edbef70f1d6bc872bf8a4d1
Reed Evertt <reed@evertt.com>
2024-03-15 12:41:58 -0700

    Outlined rest of chartStorage unit tests

--- src/lib/chartStorage.test.js
+++ src/lib/chartStorage.test.js
@@ -46,13 +46,61 @@ describe('saveChart tests', () =>
 });
 
 /* _/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾ loadAllSavedCharts tests ‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_ */
-
+describe('loadAllSavedCharts tests', () =>
+{
+    test('loads single saved chart successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+    test('loads five saved charts successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+});
 
 /* _/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾ loadSavedChart tests ‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_ */
-
+describe('loadAllSavedCharts tests', () =>
+{
+    test('loads first saved chart successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+    test('loads sixth saved chart successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+});
 
 /* _/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾ updateCurrentChartData tests ‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_ */
-
+describe('updateCurrentChartData tests', () =>
+{
+    test('saves current simple chart successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+    test('saves current complex chart successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+});
 
 /* _/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾ loadCurrentChartData tests ‾\_/‾\_/‾\_/‾\_/‾\_/‾\_/‾\_ */
-
+describe('loadCurrentChartData tests', () =>
+{
+    test('loads current simple chart successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+    test('loads current complex chart successfully', () =>
+    {
+        resetStorageDOM();
+        // Implement me!
+    });
+});
