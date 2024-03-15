1fa3f199521c024424c49bc96c2e410001cae2e4
Reed Evertt <reed@evertt.com>
2024-03-14 21:20:28 -0700

    Outlined non-sorting sortPoints tests

---
+++ src/lib/sortPoints.test.js
@@ -0,0 +1,17 @@
+/* 
+ * Unit tests for sortPoints.js
+ */
+
+// Import sortPoints module
+const sort = require('./sortPoints');
+
+// Simple non-sorting-based tests
+test("returns the same array object", () =>
+{
+    // Implement me!
+});
+
+test("doesn't change the length of the array", () =>
+{
+    // Implement me!
+});
