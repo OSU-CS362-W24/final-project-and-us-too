7e1b03b8504ede65d390ec39ec23f126ec732df9
Reed Evertt <reed@evertt.com>
2024-03-19 12:07:39 -0700

    Outlined generateChartImg() tests

---
+++ src/lib/generateChartImg.test.js
@@ -0,0 +1,16 @@
+/* 
+ * Unit tests for generateChartImg.js
+ */
+
+// Import module for testing
+const generateChart = require('./generateChartImg');
+
+test('correctly returns valid URL to image for simple chart', async () =>
+{
+    // Implement me!
+});
+
+test('correctly returns valid URL to image for complex chart', async () =>
+{
+    // Implement me!
+});
